a0518cb911d4762711a54e1baea8f03031f443cc
gleicelisbino <kellylisbino@gmail.com>
2023-11-15 19:57:36 -0300

    Remove not necessary code

--- src/main/java/org/api/serasa/dto/customer/CustomerRequestDTO.java
+++ src/main/java/org/api/serasa/dto/customer/CustomerRequestDTO.java
@@ -1,15 +1,9 @@
 package org.api.serasa.dto.customer;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import lombok.Data;
-import org.api.serasa.model.ConsultModel;
-import org.api.serasa.model.ScoreModel;
 
 import java.util.Date;
-import java.util.List;
 
 @Data
 public class CustomerRequestDTO {
@@ -20,9 +14,4 @@ public class CustomerRequestDTO {
     private String name;
     private Date birthdayDate;
 
-    @OneToOne(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
-    private ScoreModel score;
-
-    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<ConsultModel> consults;
 }
--- src/main/java/org/api/serasa/mapper/CustomerMapper.java
+++ src/main/java/org/api/serasa/mapper/CustomerMapper.java
@@ -4,7 +4,6 @@ import org.api.serasa.dto.customer.CustomerRequestDTO;
 import org.api.serasa.dto.customer.CustomerResponseDTO;
 import org.api.serasa.model.CustomerModel;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
@@ -12,11 +11,6 @@ public interface CustomerMapper {
 
     CustomerModel convertToEntity(CustomerRequestDTO customerRequestDTO);
 
-
-    @Mapping(target = "birthdayDate", source = "birthdayDate")
-    @Mapping(target = "address", source = "address")
-    @Mapping(target = "email", source = "email")
-    @Mapping(target = "name", source = "name")
     CustomerResponseDTO convertToDTO(CustomerModel customerModel);
 
     void updateEntityFromDTO(@MappingTarget CustomerModel customerModel, CustomerRequestDTO customerRequestDTO);
